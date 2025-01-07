ea16d9d4731ef1fde656180049fa60bb3627461d
basilbarge <zachb.barge@gmail.com>
2025-01-07 07:22:02 -0500

    feat: Run fzf to show directories in folders

--- main.go
+++ main.go
@@ -4,9 +4,10 @@ import (
 	"encoding/json"
 	"fmt"
 	"io/fs"
-	//"log"
 	"os"
-	//"os/exec"
+	"os/exec"
+	"slices"
+	"strings"
 )
 
 type Config struct {
@@ -16,24 +17,89 @@ type Config struct {
 func main() {
 	root := "/home/basilbarge"
 	fileSystem := os.DirFS(root)
-
-	AddDir(fileSystem, "testDir")
-
 	config := GetConfig(fileSystem)
+	
+	findArgs := config.Dirs
+	findArgs = append(findArgs, "-type", "d", "-maxdepth", "1")
+
+	find := exec.Command("find", findArgs...)
+
+	var findResult strings.Builder
+	find.Stdout = &findResult
+
+	if err := find.Run(); err != nil {
+		fmt.Printf("There was a problem running command %s. %s\n", find.Path, err)
+	}
+
+	fzf := exec.Command("fzf")
+
+	fzf.Stdin = strings.NewReader(findResult.String())
+
+	var dirBuilder strings.Builder
+	fzf.Stdout = &dirBuilder
+
+	if err := fzf.Run(); err != nil {
+		fmt.Printf("There was a problem running command %s. %s\n", fzf.Path, err)
+	}
+
+	chosenDir := dirBuilder.String()
+
+	if err := os.Chdir(chosenDir); err != nil {
+		fmt.Printf("Could not go to directory %s. %s\n", chosenDir, err)
+	}
+
+	//zellij := exec.Command("zellij")
+	fmt.Println(chosenDir)
+}
+
+func RemoveDir(filesystem fs.FS, pathToRemove string) {
+	config := GetConfig(filesystem)
+
+	if !slices.Contains(config.Dirs, pathToRemove) {
+		fmt.Println(fmt.Errorf("The current configuration does not contain %s as a directory so it cannot be removed", pathToRemove))
+		return
+	}
+
+	idxToRemove := 0
+	for idx, path := range config.Dirs {
+		if path == pathToRemove {
+			idxToRemove = idx
+			break
+		}
+	}
+
+	config.Dirs = append(config.Dirs[:idxToRemove], config.Dirs[idxToRemove+1:]...)
 
 	fmt.Println(config.Dirs)
+
+	marshaledConfig, err := json.MarshalIndent(config, "", "	")
+
+	if err != nil {
+		fmt.Printf("There was an error marshaling new config to json. %s\n", err)
+	}
+
+	err = os.WriteFile("/home/basilbarge/Documents/Projects/tmux-sessions/config.json", marshaledConfig, 0770)
+
+	if err != nil {
+		fmt.Printf("There was an error writing the new config. %s\n", err)
+	}
 }
 
 func AddDir(filesystem fs.FS, pathToAdd string) {
-	config := GetConfig(filesystem)
+	if _, err := os.Stat(pathToAdd); err != nil {
 
-	fmt.Println(config.Dirs)
+		if os.IsNotExist(err) {
+			fmt.Println("The directory you want to add does not exist on your machine. Check your spelling or try a different one!")
+		} else {
+			fmt.Printf("An error occured when searching for the directory to add. %s\n", err)
+		}
+	}
+
+	config := GetConfig(filesystem)
 
 	config.Dirs = append(config.Dirs, pathToAdd)
 
-	fmt.Println(config.Dirs)
-
-	marshaledConfig, err := json.Marshal(config)
+	marshaledConfig, err := json.MarshalIndent(config, "", "	")
 
 	if err != nil {
 		fmt.Printf("There was an error marshaling new config to json. %s\n", err)
