b8a3cb980be12116a4310c494c1b90bc279e40f2
basilbarge <zachb.barge@gmail.com>
2025-01-12 21:55:05 -0500

    fix(zellij-session): began creating function that connects to an existing session if the user chooses a session that already exists

--- zellij-session/zellij-session.go
+++ zellij-session/zellij-session.go
@@ -1,8 +1,12 @@
 package zellijSession
 
 import (
+	"fmt"
 	"io/fs"
+	"log"
 	"path/filepath"
+	"strings"
+
 	"github.com/zellijsessions/utils"
 )
 
@@ -19,9 +23,47 @@ func NewZellijSession(filesystem fs.FS) *ZellijSession {
 }
 
 func (session *ZellijSession) StartSession(directoryToStartIn string) {
+	sessionName := filepath.Base(directoryToStartIn)
+
+	if sessionExists(sessionName) {
+		session.AttachToSession(sessionName)
+		return
+	}
+
 	//change directory to the directory where you want your zellij session to start
 	utils.RunShellCommand("cd", []string{directoryToStartIn})
 
 	//run the zellij command to start a session with the name of the directory you're in
-	utils.RunShellCommand("zellij", []string{"--session", filepath.Base(directoryToStartIn)})
+	utils.RunShellCommand("zellij", []string{"--session", sessionName})
+}
+
+func (session *ZellijSession) AttachToSession(sessionName string) {
+	if !sessionExists(sessionName) {
+		log.Fatal(fmt.Errorf("Can not attach to session %s. Session does not exist", sessionName))
+	}
+
+	utils.ExecCommand("zellij", []string{"attach", sessionName}, *strings.NewReader(""))
+
+}
+
+func sessionExists(sessionName string) bool {
+	fmt.Println("Connecting to ", sessionName)
+	existingSessions := utils.ExecCommand("zellij", []string{"ls"}, *strings.NewReader(""))
+
+	outputLines := strings.Split(strings.TrimSpace(existingSessions.String()), "\n")
+
+	var existingSessionNames []string
+
+	for _, line := range outputLines {
+		existingSessionNames = append(existingSessionNames, strings.Split(line, " ")[0])
+	}
+
+	for _, existingSessionName := range existingSessionNames {
+		fmt.Println("Comparing ",sessionName, " to ", existingSessionName)
+		if existingSessionName == sessionName {
+			return true
+		}
+	}
+
+	return false
 }
