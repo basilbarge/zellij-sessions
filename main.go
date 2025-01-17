cdb3b7ddf6fe9768e89c96721028deac92c4860f
basilbarge <zachb.barge@gmail.com>
2025-01-17 09:59:55 -0500

    feat(config): Added configuration file and example as well as read from configuration file

--- main.go
+++ main.go
@@ -1,37 +1,41 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io/fs"
-	"log"
+	//"log"
 	"os"
-	"os/exec"
+	//"os/exec"
 )
 
+type Config struct {
+	Dirs []string `json:"dirs"`
+}
 
 func main() {
 	root := "/home/basilbarge"
 	fileSystem := os.DirFS(root)
-	var subdirs []fs.DirEntry
 
-	subdirs, err := fs.ReadDir(fileSystem, "Documents")
+	config := GetConfig(fileSystem)
 
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	for _, path := range subdirs {
-		fmt.Println(path)
-		command := exec.Command("tmux", "new-session", "-A", "-s", path.Name())
-		command.Stdin = os.Stdin
-		command.Stdout = os.Stdout
-		command.Stderr = os.Stderr
-
-		err := command.Run()
-
-		if err != nil {
-			log.Fatal(err)
-		}
-	}
+	fmt.Println(config.Dirs)
 }
 
+func GetConfig(fileSystem fs.FS) Config {
+	var paths Config
+
+	data, err := fs.ReadFile(fileSystem, "Documents/Projects/tmux-sessions/config.json")
+
+	if err != nil {
+		fmt.Printf("There was an error reading the configuration file. %s\n", err)
+	}
+
+	err = json.Unmarshal(data, &paths)
+
+	if err != nil {
+		fmt.Printf("There was an error unmarshalling json. %s\n", err)
+	}
+
+	return paths
+}
