fc86102455b4187e18a042f659390c8fa37a10e3
Zach Barge <zachb.barge@gmail.com>
2025-02-03 21:45:18 -0500

    feat: began adding go-arg package to add command line arguments

--- main.go
+++ main.go
@@ -2,15 +2,44 @@ package main
 
 import (
 	"fmt"
+	"os"
+
+	"github.com/alexflint/go-arg"
+
 	"github.com/charmbracelet/bubbles/list"
 	"github.com/charmbracelet/bubbletea"
 	"github.com/zellijsessions/models"
 	"github.com/zellijsessions/utils"
 	"github.com/zellijsessions/zellij-session"
-	"os"
 )
 
+type ConfigArgs struct {
+	Add    string
+	Remove string
+}
+
 func main() {
+	var args struct {
+		Config *ConfigArgs `arg:"subcommand:config"`
+	}
+
+	parser := arg.MustParse(&args)
+
+	switch {
+	case args.Config != nil:
+		if *args.Config == (ConfigArgs{}) {
+			parser.FailSubcommand("expected add or remove", "config")
+		}
+
+		if args.Config.Add == "" {
+			fmt.Println("No add")
+		}
+
+		if args.Config.Remove == "" {
+			fmt.Println("No remove")
+		}
+	}
+
 	root := "/home/basilbarge"
 	fileSystem := os.DirFS(root)
 	zellijSession := zellijSession.NewZellijSession(fileSystem)
